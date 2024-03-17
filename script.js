33fe09a8088f5f84df279ca61e530b0e5970e8ad
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-03-17 20:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ const person = new Person("John", 25);
 console.log(person.name); // Output: John
 
 
+person.age = 30;
+console.log(person.age); // Output: 30
+
 const student = new Student("Alice", 22);
 student.study(); // Output: Alice is studying
 
